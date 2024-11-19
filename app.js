349a0602ab3de9d0c7139a9f86fbd6f9c8e7baf5
Jeremie S <jeremie.seynhaeve@gmail.com>
2024-11-19 09:35:18 +0100

    form full

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+// 1. Gérez le formulaire.
+// 2. Testez les résultats.
+// 3. Ajoutez un message dans le bloc de fin en fonction des résultats.
+// 4. Ajoutez des couleurs en fonction des résultats.
+// 5. Gérez la possibilité de tentative de correction de la part de l'utilisateur (changer une valeur et re-valider).
+
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
